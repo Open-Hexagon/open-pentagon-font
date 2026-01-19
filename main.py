aa3459482e7e58c059a5a9c2cbb00f7eb662d90a
Syyrion <cuiricky4@gmail.com>
2026-01-19 17:06:11 -0500

    remove most icon prefixes; make bs icons square

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import json
 from glyph_overrides import overrides
 from constants import *
 
+
 def utf8str2ord(s: str) -> int:
     integer = int(s, 16)
     length = (integer.bit_length() + 7) // 8
@@ -31,82 +32,6 @@ font.info.openTypeOS2TypoLineGap = FONT_LINE_GAP
 
 next_private_codepoint = 0xE000
 font_lut = {}
-
-
-with open("bootstrap-icons.json", "r") as f:
-    bs_original_lut = json.load(f)
-bs_ord2codename: dict[int, str] = {
-    utf8str2ord(value): key for key, value in bs_original_lut.items()
-}
-
-bs_font = TTFont("bootstrap-icons.ttf")
-bs_head = bs_font["head"]
-bs_units_per_em = bs_head.unitsPerEm
-bs_glyph_set = bs_font.getGlyphSet()
-bs_cmap = bs_font.getBestCmap() or {}
-
-BS_ICON_SIZE = 878
-BS_PREFIX = "bs-"
-bs_scale = BS_ICON_SIZE / bs_units_per_em
-
-
-for codepoint, name in bs_cmap.items():
-    glyph_name = BS_PREFIX + name
-    if glyph_name in font:
-        continue
-
-    dst = font.newGlyph(glyph_name)
-    dst.width = FONT_EM
-
-    dst_pen = dst.getPointPen()
-    transform = Identity.scale(bs_scale, bs_scale).translate(
-        (FONT_EM - BS_ICON_SIZE) / 2 / bs_scale,
-        (FONT_DESCENDER + (FONT_EM - BS_ICON_SIZE) / 2) / bs_scale,
-    )
-    dst_pen2 = TransformPointPen(dst_pen, transform)
-    bs_glyph_set[name].drawPoints(dst_pen2)
-
-    font[glyph_name].unicodes.append(next_private_codepoint)
-    font_lut[BS_PREFIX + bs_ord2codename[codepoint]] = ord2utf8str(
-        next_private_codepoint
-    )
-    next_private_codepoint += 1
-
-bs_font.close()
-
-
-with open("promptfont-glyphs.json", "r") as f:
-    pf_original_list = json.load(f)
-pf_ord2codename: dict[int, str] = {
-    d["codepoint"]: d["code-name"] for d in pf_original_list
-}
-
-pf_font = TTFont("promptfont.ttf")
-pf_head = pf_font["head"]
-pf_units_per_em = pf_head.unitsPerEm
-pf_glyph_set = pf_font.getGlyphSet()
-pf_cmap = pf_font.getBestCmap() or {}
-
-PF_PREFIX = "pf-"
-
-for codepoint, name in pf_cmap.items():
-    glyph_name = PF_PREFIX + (pf_ord2codename.get(codepoint) or name)
-    if glyph_name in font:
-        continue
-
-    dst = font.newGlyph(glyph_name)
-    src_width, _ = pf_font["hmtx"][name]
-    dst.width = src_width
-
-    dst_pen = dst.getPointPen()
-    pf_glyph_set[name].drawPoints(dst_pen)
-
-    font[glyph_name].unicodes.append(next_private_codepoint)
-    font_lut[glyph_name] = ord2utf8str(next_private_codepoint)
-    next_private_codepoint += 1
-
-pf_font.close()
-
 
 sq_font = TTFont("OpenSquare.ttf")
 sq_head = sq_font["head"]
@@ -131,6 +56,86 @@ for codepoint, name in sq_cmap.items():
 
     font[name].unicodes.append(codepoint)
 
+sq_font.close()
+
+with open("bootstrap-icons.json", "r") as f:
+    bs_original_lut = json.load(f)
+bs_ord2codename: dict[int, str] = {
+    utf8str2ord(value): key for key, value in bs_original_lut.items()
+}
+
+bs_font = TTFont("bootstrap-icons.ttf")
+bs_head = bs_font["head"]
+bs_units_per_em = bs_head.unitsPerEm
+bs_glyph_set = bs_font.getGlyphSet()
+bs_cmap = bs_font.getBestCmap() or {}
+
+BS_ICON_SIZE = 880
+BS_PREFIX = "bs-"  # used only in case of name collision
+bs_scale = BS_ICON_SIZE / bs_units_per_em
+
+
+for codepoint, name in bs_cmap.items():
+    glyph_name = name
+    if glyph_name in font:
+        glyph_name = BS_PREFIX + glyph_name
+        if glyph_name in font:
+            print("bootstrap name collision", glyph_name)
+            continue
+
+    dst = font.newGlyph(glyph_name)
+    # icons must be square or weird pixel aligment stuff happens
+    dst.width = FONT_ACTUAL_HEIGHT
+
+    dst_pen = dst.getPointPen()
+    transform = Identity.scale(bs_scale, bs_scale).translate(
+        (dst.width - BS_ICON_SIZE) / 2 / bs_scale,
+        (FONT_DESCENDER + (FONT_EM - BS_ICON_SIZE) / 2) / bs_scale,
+    )
+    dst_pen2 = TransformPointPen(dst_pen, transform)
+    bs_glyph_set[name].drawPoints(dst_pen2)
+
+    font[glyph_name].unicodes.append(next_private_codepoint)
+    font_lut[glyph_name] = ord2utf8str(next_private_codepoint)
+    next_private_codepoint += 1
+
+bs_font.close()
+
+
+with open("promptfont-glyphs.json", "r") as f:
+    pf_original_list = json.load(f)
+pf_ord2codename: dict[int, str] = {
+    d["codepoint"]: d["code-name"] for d in pf_original_list
+}
+
+pf_font = TTFont("promptfont.ttf")
+pf_head = pf_font["head"]
+pf_units_per_em = pf_head.unitsPerEm
+pf_glyph_set = pf_font.getGlyphSet()
+pf_cmap = pf_font.getBestCmap() or {}
+
+PF_PREFIX = "pf-"  # used only in case of name collision
+
+for codepoint, name in pf_cmap.items():
+    glyph_name = pf_ord2codename.get(codepoint) or name
+    if glyph_name in font:
+        glyph_name = PF_PREFIX + glyph_name
+        if glyph_name in font:
+            print("bootstrap name collision", glyph_name)
+            continue
+
+    dst = font.newGlyph(glyph_name)
+    src_width, _ = pf_font["hmtx"][name]
+    dst.width = src_width
+
+    dst_pen = dst.getPointPen()
+    pf_glyph_set[name].drawPoints(dst_pen)
+
+    font[glyph_name].unicodes.append(next_private_codepoint)
+    font_lut[glyph_name] = ord2utf8str(next_private_codepoint)
+    next_private_codepoint += 1
+
+pf_font.close()
 
 # Compile and save as TTF
 ttf = compileTTF(font)
